2dd575ffb5ebcaec76e0723ced736108a44c4bbd
Ultrafeel <temponello@mail.ru>
2017-01-24 17:55:37 +0300

    caller client

--- caller_client/src/caller_client.cpp
+++ caller_client/src/caller_client.cpp
@@ -5,9 +5,13 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-#include <CSettingsReader.h>
-
 #include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <CSettingsReader.h>
+using namespace boost;
+using boost::asio::ip::tcp;
+
 using namespace std;
 
 int main() {
@@ -20,11 +24,14 @@ int main() {
         return 1;
     }
     auto itPort = cfg.m_parsedInt.find("port");//[;
+    auto itPortStr = cfg.m_parsed.find("port");//[;
     //auto search = example(2);
     if(itPort == cfg.m_parsedInt.end()) { //cfg.m_file.rdbuf()->
         std::cout << " cfg wrong . Port not Found in : " << cfgP << " "   << '\n';
         return 1;
     }
+
+   // cfg.m_parsedInt.find("port")
      auto itServAddr = cfg.m_parsed.find("server_addr");//[;
     //auto search = example(2);
     if(itServAddr == cfg.m_parsed.end()) { //cfg.m_file.rdbuf()->
@@ -33,9 +40,17 @@ int main() {
     }
     cout << " cfg  Server  to use   : " <<  itServAddr->second << " "   << '\n';
 
-    cout << " cfg  Port  to use   : " <<  itPort->second << " "   << '\n';
+    cout << " cfg  Port  to use   : " <<  itPort->second << " (" << itPortStr->second  << ")\n";
+
+    asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(itServAddr->second, itPortStr->second);
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
 
 
-
-	return 0;
+    return 0;
 }
