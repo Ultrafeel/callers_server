2bb436f72bed2b61a61cdef81904267aa4e30ffa
Ultrafeel <temponello@mail.ru>
2017-02-14 10:54:20 +0300

    PostClearUser - why access violation?

--- caller_server/call_executor.h
+++ caller_server/call_executor.h
@@ -33,7 +33,7 @@ struct CompaniesSorter
 //protected:
     int compar (CCompanyTask const & t1, CCompanyTask const & t2) const
     {
-         if (t1.m_priority < t2.m_priority)
+        if (t1.m_priority < t2.m_priority)
             return 1;
         else if(t1.m_priority != t2.m_priority)
             return 0;
@@ -186,20 +186,23 @@ class caller_executor_pool: public executor_pool_base
 
     }
     friend class call_server;
+
 public:
 
     static char  const * endMessage ;//= "\n All tasks end";
-    void join(CInteractor_ptr participant)
+    void join(CInteractor_ptr )
     {
+        //participant
         using namespace std;
-        cout << __FUNCTION__ << " join" << endl;
+        cout << __FUNCTION__ << " ." << endl;
 
     }
 
-    void leave(CInteractor_ptr /*participant*/)
+    void leave(CInteractor_ptr participant)
     {
         using namespace std;
         cout << __FUNCTION__ << " ." << endl;
+        PostClearUser(participant);
     }
 
     void deliver_to_client(CInteractor_ptr client, CServerStatus const& msg) override
@@ -244,12 +247,12 @@ public:
         m_call_executor.Proc(task1);
 
     }
-    std::set<CTask_to_handle, CHandleTaskSorter> m_incomplete;
     void CheckIfLeft(CTask_to_handle tsk) override
     {
         m_io_service.post(boost::bind(&caller_executor_pool::CheckIfLeftAndLoadNext, this, tsk));
     }
-
+private:
+    std::set<CTask_to_handle, CHandleTaskSorter> m_incomplete;
     void CheckIfLeftAndLoadNext(CTask_to_handle tsk)
     {
         bool tasks_left = false;
@@ -331,13 +334,42 @@ public:
 //                      boost::bind(&client_app::deliver, _1, boost::ref(msg)));
     }
 
+
+    void PostClearUser(CInteractor_ptr tsk)
+    {
+        m_io_service.post(boost::bind(&caller_executor_pool::ClearUser, this, tsk));
+    }
+
+    void ClearUser(CInteractor_ptr intr)
+    {
+
+        std::lock_guard<std::mutex> lock(m_queue_m);
+        read_queue.eraseAllOfInteractor(intr);
+
+        for(auto itTh  = m_incomplete.begin(); itTh != m_incomplete.end(); )
+        {
+            if (itTh->m_client == intr)
+            {
+                //tasks_left = true;
+                auto itL = m_incomplete.erase(itTh);
+                itTh = itL;
+
+            }
+            else
+                ++itTh;
+        }
+    }
+
 private:
 //client_app_ptr  m_client_app;
 //holds pointers.
-    //  std::set<CInteractor_ptr> m_client_sessions;
+//  std::set<CInteractor_ptr> m_client_sessions;
 // std::set<CInteractor_weak_ptr> m_client_sessions;
 // enum { max_recent_msgs = 100 };
+
+//todo:maybe better to use strand.
     std::mutex m_queue_m;
+
     typedef    std::priority_queue<CTask_to_handle, std::deque<CTask_to_handle>, CHandleTaskSorter>
     TCont;
 
@@ -348,13 +380,40 @@ private:
         {
             return c;
         }
-    };
+        struct FunctIsOfIteractor
+        {
+            CInteractor_ptr intr;//isIteractors
+            bool operator ()(CTask_to_handle const& th) const
+            {
+                return (th.m_client == intr);
+            }
+        };
+        void eraseAllOfInteractor(CInteractor_ptr intr)
+        {
+            auto & cont = c;//read_queue.GetCont();
+            auto initCount = cont.size();
+            //at cont.find();
+            FunctIsOfIteractor fit = {intr};
+            std::remove_if(cont.begin(), cont.end(), fit);
+//            for(auto itTh  = cont.begin(); itTh != cont.end(); ++itTh)
+//            {
+//                if (itTh->m_client == intr)
+//                {
+//                    //tasks_left = true;
+//                    cont.erase(itTh);
+//                }
+//            }
+            if (initCount != cont.size())
+                std::make_heap(cont.begin(), cont.end(), comp);//maybe useless
+
+        }
+     };
     UnProtectCont read_queue;
 
 
 
 // std::set<CCompanyTask, CompaniesSorter>
-    // std::set<CTask_to_handle,  CHandleTaskSorter>    read_queue;
+// std::set<CTask_to_handle,  CHandleTaskSorter>    read_queue;
 // server_status_queue recent_msgs_;
     boost::asio::io_service & m_io_service;
     call_executor m_call_executor;
